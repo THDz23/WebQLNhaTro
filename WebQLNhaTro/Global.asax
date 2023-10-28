5351e545a7da93b815372a2eef9ff657989c3a4a
Hùng Trịnh <trinhhung23102003@gmail.com>
2023-10-28 09:21:33 +0700

    Add project files.

---
+++ WebQLNhaTro/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebQLNhaTro.MvcApplication" Language="C#" %>
---
+++ WebQLNhaTro/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebQLNhaTro.Startup))]
+namespace WebQLNhaTro
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
