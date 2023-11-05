82ff2f427585816c01c42188b88225f8f1308b20
Hùng Trịnh <trinhhung23102003@gmail.com>
2023-11-05 08:27:27 +0700

    hien thi trang tim phong va chuc nang loc sap xep

--- WebQLNhaTro/Controllers/MotelsController.cs
+++ WebQLNhaTro/Controllers/MotelsController.cs
@@ -13,11 +13,32 @@ namespace WebQLNhaTro.Controllers
         // GET: Motels
         public ActionResult Index()
         {
-            return View();
+            ViewBag.KhuVuc = new SelectList(db.areas.ToList(), "AreaID", "ProvinceName");
+            ViewBag.GiaTu = new SelectList(db.searchprices.ToList(), "ID", "PriceFrom");
+            ViewBag.Danhmuc = new SelectList (db.CategoryMotels.ToList(),"CategoryID","type");
+            var item = db.motels;
+            return View(item);
         }
+        
         public ActionResult Detail(int id)
         {
             var item = db.motels.Where(x=>x.MotelID == id).Single();
+            return View(item);
+        }
+        
+        public ActionResult AscendingPrice()
+        {
+            var item = db.motels.OrderBy(x => x.Price).ToList();
+            return View(item);
+        }
+        public ActionResult DecreasePrice()
+        {
+            var item = db.motels.OrderByDescending(x => x.Price).ToList();
+            return View(item);
+        }
+        public ActionResult DateNew()
+        {
+            var item = db.motels.OrderBy(x => x.CreateDate).ToList();
             return View(item);
         }
     }
