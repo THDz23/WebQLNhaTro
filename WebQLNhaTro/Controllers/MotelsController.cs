848ad8132eaca7d67b75d4eb9659899c1be5b0d7
Hùng Trịnh <trinhhung23102003@gmail.com>
2023-11-06 11:18:53 +0700

    Chức năng search sử dụng autocomplete

--- WebQLNhaTro/Controllers/MotelsController.cs
+++ WebQLNhaTro/Controllers/MotelsController.cs
@@ -11,15 +11,92 @@ namespace WebQLNhaTro.Controllers
     {
         NhaTroEntities db = new NhaTroEntities();
         // GET: Motels
-        public ActionResult Index()
+        [HttpGet]
+        public ActionResult Index(string keyword,int Danhmuc = 0,int KhuVuc=0,int giatu = 0)
         {
             ViewBag.KhuVuc = new SelectList(db.areas.ToList(), "AreaID", "ProvinceName");
             ViewBag.GiaTu = new SelectList(db.searchprices.ToList(), "ID", "PriceFrom");
             ViewBag.Danhmuc = new SelectList (db.CategoryMotels.ToList(),"CategoryID","type");
-            var item = db.motels;
-            return View(item);
+            IOrderedQueryable<motel> item = db.motels;
+            if (!string.IsNullOrEmpty(keyword) && Danhmuc == 0 && KhuVuc == 0 && giatu == 0)
+            {
+                item = item.Where(x => x.Title.Contains(keyword)).OrderBy(x=>x.Price);
+                return View(item);
+            }
+            else if (!string.IsNullOrEmpty(keyword) && Danhmuc != 0 && KhuVuc != 0 && giatu == 0)
+            {
+                    item = item.Where(x => x.Title.Contains(keyword) && x.CategoryID == Danhmuc && x.AreaID == KhuVuc).OrderBy(x => x.Price);
+                    return View(item);
+            }
+            else if (!string.IsNullOrEmpty(keyword) && Danhmuc != 0 && KhuVuc == 0 && giatu != 0)
+            {
+                item = item.Where(x => x.Title.Contains(keyword) && x.CategoryID == Danhmuc && x.ID == giatu).OrderBy(x => x.Price);
+                return View(item);
+            }
+            else if (!string.IsNullOrEmpty(keyword) && Danhmuc != 0 && KhuVuc != 0 && giatu != 0)
+            {
+                item = item.Where(x => x.Title.Contains(keyword) && x.CategoryID == Danhmuc && x.AreaID == KhuVuc && x.ID == giatu).OrderBy(x => x.Price);
+                return View(item);
+            }
+            else if (!string.IsNullOrEmpty(keyword) && Danhmuc == 0 && KhuVuc != 0 && giatu != 0)
+            {
+                item = item.Where(x => x.Title.Contains(keyword) && x.AreaID  == KhuVuc && x.ID == giatu).OrderBy(x => x.Price);
+                return View(item);
+            }
+            else if (!string.IsNullOrEmpty(keyword) && Danhmuc != 0 && KhuVuc == 0 && giatu == 0)
+            {
+                item = item.Where(x => x.Title.Contains(keyword) && x.CategoryID == Danhmuc).OrderBy(x => x.Price);
+                return View(item);
+            }
+            if (Danhmuc != 0 && KhuVuc == 0 && giatu == 0)
+            {
+                item = (IOrderedQueryable<motel>)item.Where(x => x.CategoryID == Danhmuc);
+                return View(item.ToList());
+            }
+            else if (Danhmuc != 0 && KhuVuc != 0 && giatu == 0)
+            {
+                item = (IOrderedQueryable<motel>)item.Where(x => x.CategoryID == Danhmuc && x.AreaID == KhuVuc);
+                return View(item.ToList());
+            }
+            else if(Danhmuc != 0 && KhuVuc != 0 && giatu != 0)
+            {
+                item = (IOrderedQueryable<motel>)item.Where(x => x.CategoryID == Danhmuc && x.AreaID == KhuVuc && x.ID == giatu);
+                return View(item.ToList());
+            }
+            else if (KhuVuc != 0 && giatu != 0 && Danhmuc == 0)
+            {
+                item = (IOrderedQueryable<motel>)item.Where(x => x.ID == giatu && x.AreaID == KhuVuc);
+                return View(item.ToList());
+            }
+            else if (KhuVuc != 0 && giatu == 0 && Danhmuc == 0)
+            {
+                item = (IOrderedQueryable<motel>)item.Where(x => x.AreaID == KhuVuc);
+                return View(item.ToList());
+            }
+            else if (KhuVuc == 0 && giatu != 0 && Danhmuc == 0)
+            {
+                item = (IOrderedQueryable<motel>)item.Where(x => x.ID == giatu);
+                return View(item.ToList());
+            }
+            else if(Danhmuc != 0 && giatu != 0 && KhuVuc == 0)
+            {
+                item = (IOrderedQueryable<motel>)item.Where(x => x.ID == giatu && x.CategoryID == Danhmuc);
+                return View(item.ToList());
+            }
+            else
+            {
+                return View(item);
+            }
         }
-        
+
+        public JsonResult GetByName(string keyword)
+        {
+            var allsearch = db.motels.Where(x => x.Title.Contains(keyword)).Select(x => new Seach { 
+                label = x.Title,
+                val = x.MotelID.ToString()
+            }).ToList();
+            return new JsonResult { Data = allsearch, JsonRequestBehavior =  JsonRequestBehavior.AllowGet };
+        }
         public ActionResult Detail(int id)
         {
             var item = db.motels.Where(x=>x.MotelID == id).Single();
---
+++ WebQLNhaTro/Models/Seach.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebQLNhaTro.Models
+{
+    public class Seach
+    {
+        public string label { get; set; }
+        public string val { get; set; } 
+    }
+}
